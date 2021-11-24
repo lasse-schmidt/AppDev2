bd7028a6626afd0a3ecda1738103c203ee443bee
Lasse Schmidt <lass8465@edu.sde.dk>
2021-11-24 10:17:03 +0100

    Menu fix

---
+++ AppDev2/AppDev2/ViewModels/CamViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AppDev2.ViewModels
+{
+    public class CamViewModel : BaseViewModel
+    {
+        public CamViewModel()
+        {
+            Title = "Cam";
+        }
+    }
+}
